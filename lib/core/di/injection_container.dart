4c06616a55fd0f4406c33722218b1c273acd00f2
tajjallee <TajjalleeAbid@gmail.com>
2025-10-03 19:12:38 +0400

    http client added

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,7 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
+import '../network/api_client.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  // External
+  sl.registerLazySingleton(() => http.Client());
+  
 }
